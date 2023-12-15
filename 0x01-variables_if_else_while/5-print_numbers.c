f21d87014ea3082e7948ff101076ac0cb25e8126
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-16 07:32:25 +0800

    all the single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d\n", n);
+	return (0);
+}
