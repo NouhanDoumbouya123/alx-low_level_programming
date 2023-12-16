c5c0cbf518027c43ee47f1c9ec89e66b7da0b2fa
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-16 08:32:52 +0800

    numbers separated with , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all the possible combinations of single digits numbers
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
