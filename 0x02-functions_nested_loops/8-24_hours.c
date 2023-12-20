56412dcadb76168ef8268821e2df90e16227920f
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-20 08:45:39 +0800

    bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,18 +16,21 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 3; j++)
 		{
-		for (k = 0; k <= 5; k++)
-		{
-		for (l = 0; l <= 9; l++)
-		{
-		_putchar(i + '0');
-		_putchar(j + '0');
-		_putchar(':');
-		_putchar(k + '0');
-		_putchar(l + '0');
-		_putchar('\n');
-		}
-		}
+			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
+			{
+				for (k = 0; k <= 5; k++)
+				{
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
 		}
 	}
 }
