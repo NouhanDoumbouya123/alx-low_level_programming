7b98e1d4aa54f85be5152bb1597ad35896cebe5b
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-20 09:46:51 +0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * add - sum of two integers
+ *
+ * Description: prints the sum of two integers
+ *
+ * @c: the first integer to be printed
+ * @d: the second integer to be printed
+ *
+ * Return: Always add;
+ */
+
+int add(int c, int d)
+{
+	int n;
+
+	n = c + d;
+
+	_putchar(n + '0');
+	return (n);
+
+}
