d1e747f1b601e4fe5059bac8cd14393e28fbdbe2
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-20 10:35:30 +0800

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,28 +13,36 @@
 
 void print_to_98(int n)
 {
-
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			printf(",");
-			printf(" ");
-		}
-	}
-	else if (n == 98)
+	for (; n <= 98; n++)
 	{
-		printf("%d", n);
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
 	}
 	else
 	{
-		for (;  n >= 98; n--)
-		{
-			printf("%d", n);
-			printf(",");
-			printf(" ");
-		}
+	printf("%d, ", n);
 	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
 	printf("\n");
+	break;
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+	}
+	}
 }
