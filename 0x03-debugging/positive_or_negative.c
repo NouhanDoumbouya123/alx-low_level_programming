d29a42c197c6484169bd9b94ed51b21541bfc7b6
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-21 09:14:32 +0800

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - prints a random number decide whether that number is positive,
+ * positive_or_negative - prints a random number decide
+ * whether that number is positive,
  * negative or zero
- *
+ *@i: the parameter to pass to the function
  * Return: Always 0
  */
 
 void positive_or_negative(int i)
 {
-        int n = i;
+	int n = i;
 
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
-	printf("\n");
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 }
