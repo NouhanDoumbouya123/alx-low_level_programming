e433aaafbc4171d3028fe560e7af32de1f22f2a3
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-21 08:01:39 +0800

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,20 @@
-#include "main.h"
+#include "main.h" // Assuming necessary headers are in main.h
 
+void print_times_table(int num) {
+    // Your logic to print the times table for the given 'num'
+    // For example:
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+}
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-
-
-int print_times_table()
-{
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
+int main() {
+    print_times_table(3);
+    printf("\n");
+    print_times_table(5);
+    printf("\n");
+    print_times_table(98);
+    printf("\n");
+    print_times_table(12);
+    return 0;
 }
