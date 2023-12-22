f8ed3a6a63cc2147450d3627ad086cb01c7e1a2f
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-22 12:33:37 +0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ *
+ * @size: the size of the square
+ *
+ * Return: Always (0)
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 1; i <= size; i++)
+	{
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
