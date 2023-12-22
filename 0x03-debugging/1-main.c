a67255ea70a6c551c3d4e3420076020946ae377a
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2023-12-22 22:12:12 +0800

    the case of 2000

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,19 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
+/**
+ * while (i < 10)
+ * @'\\o/\n": kdieke
+ * {
+ * putchar(i);
+ * }
+ */
 
-       // while (i < 10) this is the part causing an infinite loop
-        {
-		putchar(i);
-        }
+	printf("Infinite loop avoided! \\o/\n");
 
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	return (0);
 }
