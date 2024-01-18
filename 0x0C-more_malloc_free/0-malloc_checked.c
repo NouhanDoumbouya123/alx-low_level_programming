958534e54aa15b77bae0672455cffc5732592ae9
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-01-19 00:10:17 +0800

    first task solved

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,11 @@ void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
-	ptr = malloc(b * sizeof(int));
+	ptr = malloc(b * sizeof(unsigned int));
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
 	else
 		return (ptr);
-	free(ptr);
 }
