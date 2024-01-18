6f787e04db99c2bca9e457dc85298851f6bb7f1f
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-01-18 23:42:04 +0800

    first task solved

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  * malloc_checked - allocates memory using malloc and check if success
  * @b: the parameter
- * return: if fails, it should return exit(98)
+ * Return: if fails, it should return exit(98)
  */
 
 void *malloc_checked(unsigned int b)
