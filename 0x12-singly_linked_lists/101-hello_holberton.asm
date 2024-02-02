d237e3a1d2e82de03a7373a7225f8fc225050b11
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-02-02 10:38:32 +0800

    using assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,10 @@
-section .data
-    hello db 'Hello, Holberton', 0
-    newline db 10  ; ASCII code for newline character
-
-section .text
-    global main
-
-extern printf
-extern exit
-
+global    main
+          extern    printf
 main:
-    ; write "Hello, Holberton" to stdout
-    mov rdi, 1
-    mov rsi, hello
-    mov rdx, 17  ; Length of the string
-    call printf
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-    ; write newline character to stdout
-    mov rdi, 1
-    mov rsi, newline
-    mov rdx, 1
-    call printf
-
-    ; exit program
-    mov rdi, 0
-    call exit
