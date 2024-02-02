e387de37218e0bd9b586c6b2831f25721a463f8c
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-02-02 10:54:38 +0800

    singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,13 +29,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (temp == NULL)
 		*head = newnode;
-		return (newnode);
 
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-			temp->next = newnode;
-		}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		temp->next = newnode;
+	}
 
 	return (*head);
 }
