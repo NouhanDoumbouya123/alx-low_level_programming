6f08849e8fd5e8384cadefc9b7b3789145e59a93
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-02-02 08:59:03 +0800

    freeing the list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the head of the least
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+	free(current->str);
+	free(current);
+
+	current = next;
+	}
+}
